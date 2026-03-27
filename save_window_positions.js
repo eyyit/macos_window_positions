400cda0f630918e5c0c4d0f129acd0548987d426
Elliott Friedman <elliott.friedman@gmail.com>
2026-03-26 21:25:42 -0400

    Revert "Refactor window position saving using CoreGraphics"
    This reverts commit 26d667cac4d5b551d7125d136e7a7aeaecdbad05.

--- save_window_positions.js
+++ save_window_positions.js
@@ -1,6 +1,3 @@
-// Import the CoreGraphics framework to see across all macOS Spaces
-ObjC.import('CoreGraphics');
-
 // Default filepath for all scripts.
 var home_dir = ObjC.unwrap($.NSHomeDirectory()).toString();
 var filename = home_dir + '/.saved_window_positions';
@@ -9,42 +6,23 @@ var app = Application.currentApplication();
 app.includeStandardAdditions = true;
 var saved_positions = {};
 
-// Use CoreGraphics to pull a list of ALL windows across ALL spaces
-var windowList = $.CGWindowListCopyWindowInfo($.kCGWindowListOptionAll, $.kCGNullWindowID);
-var windows = ObjC.deepUnwrap(windowList);
+// Get a list of all visible application's names
+var app_list = Application("System Events").applicationProcesses.whose({visible: true}).name();
 
-// Get a list of all visible applications to filter out background processes
-var visible_apps = Application("System Events").applicationProcesses.whose({visible: true}).name();
-var visible_apps_set = new Set(visible_apps); // Converted to a Set for faster lookup
+// Loop through all applications
+for (app_num in app_list) {
+  var app_name = app_list[app_num];
+  saved_positions[app_name] = {};
 
-// Loop through the CoreGraphics window list
-for (var i = 0; i < windows.length; i++) {
-    var win = windows[i];
-    var layer = win.kCGWindowLayer;
-    var appName = win.kCGWindowOwnerName;
-    
-    // Layer 0 denotes standard application windows. 
-    // We also check if the app is actually visible/running.
-    if (layer === 0 && visible_apps_set.has(appName)) {
-        
-        // Ensure the app object exists in our save state
-        if (!saved_positions[appName]) {
-            saved_positions[appName] = {};
-        }
+  // Get a list of all windows for the current application
+  var window_list = Application(app_name).windows;
 
-        // Handle cases where a window has no title
-        var windowName = win.kCGWindowName || ("Unnamed_Window_" + win.kCGWindowNumber);
-        var bounds = win.kCGWindowBounds;
-
-        // Map CoreGraphics bounds (X, Y) to standard JXA bounds format (x, y) 
-        // so your restore script doesn't break
-        saved_positions[appName][windowName] = {
-            "x": bounds.X,
-            "y": bounds.Y,
-            "width": bounds.Width,
-            "height": bounds.Height
-        };
-    }
+  // loop through all windows for the current application
+  for (window_num in window_list) {
+    var window = window_list[window_num];
+    var window_name = window_list[window_num].name();
+    saved_positions[app_name][window_name] = window.bounds();
+  }
 }
 
 // Open a new file, overwrite if needed
